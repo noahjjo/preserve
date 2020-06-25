86c29659c40b0df2ed285090a0442799b6a6f32d
= <=>
2020-06-25 06:58:43 +0300

    website

---
+++ index.php
@@ -0,0 +1,191 @@
+
+    <!DOCTYPE html>
+      <html lang="en">
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+      <head>
+        <base  />
+        <meta charset="UTF-8" />
+    	   <link href="css/style.css" rel="stylesheet" type="text/css">
+    	    <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+          <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+          <!-- Scripts For Layer Slider  -->
+          <script src="layerslider/js/greensock.js" type="text/javascript"></script>
+
+          <!-- LayerSlider script files -->
+          <script src="layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
+          <script src="layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+          <script>
+            $(document).ready(function(){
+            jQuery("#layerslider").layerSlider({
+            	responsive: true,
+            	responsiveUnder: 1170,
+            	layersContainer: 870,
+            	skin: 'fullwidth',
+            	hoverPrevNext: true,
+            	skinsPath: 'layerslider/skins/'
+            });
+            });
+          </script>
+          <title>Home || Preserve</title>
+      </head>
+    <body>
+      <section id="topbar" class="d-none d-lg-block">
+        <div class="container clearfix">
+          <div class="contact-info float-left">
+            <i class="fa fa-envelope-o"></i> <a href="mailto:info@preserve.ie">info@preserve.ie</a>
+            <i class="fa fa-phone"></i> +353 85 214 5155
+          </div>
+          <div class="social-links float-right">
+            <a href="https://www.twitter.com" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="https://www.facebook.com" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="https://www.instagram.com" class="instagram"><i class="fa fa-instagram"></i></a>
+            <a href="https://www.google-plus.com" class="google-plus"><i class="fa fa-google-plus"></i></a>
+            <a href="https://www.linkedin.com" class="linkedin"><i class="fa fa-linkedin"></i></a>
+          </div>
+        </div>
+      </section>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky">
+        <a class="navbar-brand" href="">Preserve</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" d
+        ata-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+         aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="index.php#faq">FAQs</a>
+              </li>
+            </li>
+          </ul>
+        </div>
+    </nav>
+    <div class="container">
+      <div class="row" style="margin-bottom:20px;margin-top:20px;">
+        <div class="col-12">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="images/slide1.png" style="width:auto;height:400px;" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="images/slide2.png" style="width:auto;height:400px;" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="container" style="margin-bottom:20px;">
+      <div class="row">
+        <div class="col-12">
+          <h5 class="heading">Educating the children on how to use social media rightly</h5>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row" style="margin-bottom:20px;">
+        <div class="col-12">
+          <p>
+            Preserve is an Ireland&rsquo;s Social enterprise that was born with the objective to empower
+            vulnerable groups, especially children and teenagers to use social media wisely.
+            Social media is attributed to rising rates of anxiety, depression and
+            self-harm witnessed among youth in Ireland.
+            Preserve&rsquo;s solution is
+            to educate every school child under the digital age of consent
+            (13-16 years old) through in both formal and informal
+            ways through gamification and learning.
+            This is to encouraging
+            the responsible and mindful use of social media in the hands of
+            children that are exposed to these platforms building their
+            resilience navigating through them both now and the future.
+          </p>
+        </div>
+      </div>
+      <div class="row" style="margin-bottom:20px;">
+        <div class="col-12">
+          <h6 class="heading">The Executive Team</h6>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col-3">
+            <img src="images/director.jpg" alt="" style="width:100%;height:200px;">
+            <h6 class="sub_heading">Adeyeme Olajide</h6>
+            <h6>Director</h6>
+          </div>
+          <div class="col-3">
+            <img src="images/peter.png" alt="" style="width:100%;height:200px;">
+            <h6 class="sub_heading">Peter Bruno Habimana</h6>
+          </div>
+          <div class="col-3">
+            <img src="images/pablo.png" alt="" style="width:100%;height:200px;">
+            <h6 class="sub_heading">Pablo Armas</h6>
+          </div>
+          <div class="col-3">
+            <img src="images/dominyka.jpg" alt="" style="width:100%;height:200px;">
+            <h6 class="sub_heading">Dominyka Majauskaite</h6>
+          </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row" style="margin-bottom:20px;margin-top:20px;">
+        <div class="col-12">
+          <h6 class="heading">Launching Soon</h6>
+        </div>
+      </div>
+    </div>
+    <div class="container" id="faq">
+      <div class="row" style="margin-bottom:20px;margin-top:20px;">
+        <div class="col-12">
+          <h6 class="heading">Frequently Asked Questions</h6>
+          <div class="accordion" id="accordionExample">
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Question One
+                  </button>
+                </h2>
+              </div>
+
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Preserve</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://nt-host.com/">NT-HOST</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
